b09ab6c7ebdec0d8699e48a88cb09361b923b1f4
DevLayth <124042283+DevLayth@users.noreply.github.com>
2023-11-10 14:12:24 +0300

    socket connection

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express=require('express');
-const router=myRouter=require('./routes/locations');
+const myRouter=require('./routes/locations');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 const cors=require('cors');
 const socketio=require('socket.io');
 const http=require('http');
+
 
 const app=express();
 const server=http.createServer(app);
@@ -17,9 +18,19 @@ app.use(cors());
 //bodyParser middle were
 app.use(bodyParser.json());
 
-
 //Routes middle were
 app.use('/locations',myRouter);
+
+
+io.on('connection', (client) => {
+  console.log('A new client connected. ID:', client.id);
+
+  client.on('disconnect', () => {
+    console.log('Client disconnected. ID:', client.id);
+  });
+
+});
+
 
 mongoose.connect('mongodb+srv://laythhikmat20:MKMnA30cIsBWMtzT@devlayth.bmzd9p5.mongodb.net/uodDB?retryWrites=true&w=majority');
 
@@ -30,10 +41,10 @@ db.once('open', () => {
   console.log('Connected to the MongoDB database');
 server.listen(process.env.PORT||8080,()=>{
   
-    console.log('server is running on port '+process.env.PORT);
+    console.log('server is running on port 8080');
 });
 });
 
-x
+
 
 
