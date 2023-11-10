df06b7e8d42219126a94a42765b5990da2d74779
DevLayth <124042283+DevLayth@users.noreply.github.com>
2023-11-10 10:24:35 +0300

    socket init

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const router=myRouter=require('./routes/locations');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
 const cors=require('cors');
+const socketio=require('socket.io');
+const http=require('http');
 
 const app=express();
+const server=http.createServer(app);
+const io=socketio(server);
 
 //Allow access middle were
 app.use(cors());
@@ -24,9 +28,12 @@ const db = mongoose.connection;
 
 db.once('open', () => {
   console.log('Connected to the MongoDB database');
-app.listen(process.env.PORT||8080,()=>{
+server.listen(process.env.PORT||8080,()=>{
+  
     console.log('server is running on port '+process.env.PORT);
 });
 });
+
+x
 
 
